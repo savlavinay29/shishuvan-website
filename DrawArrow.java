83f80a5bface1b0743f1ab3b7abc646613cbe564
savlavinay29 <savlavinay022@gmail.com>
2021-05-25 00:15:30 +0530

    Add files via upload

---
+++ DrawArrow.java
@@ -0,0 +1,10 @@
+   import java.awt.Graphics;
+   import java.applet.Applet;
+ 
+   public class DrawArrow extends Applet
+   {	
+     public void paint(Graphics g)
+     {
+       g.drawString("Hello World!",20,20);
+    }
+   }
